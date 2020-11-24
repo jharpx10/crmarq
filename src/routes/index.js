322793dbcdb89141739ab9ed0de3dd79fe3c0b76
david ballesteros <david.ballesteros@udea.edu.co>
2020-11-23 22:01:55 -0500

    Vista principal

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
+const { isLoggedIn, isNotLoggedIn } = require('../lib/auth');
+
 const pool = require('../../database/database');
 
-router.get('/', (req, res) => {
-    res.send('Hello f world');
+router.get('/', isNotLoggedIn, (req, res) => {
+    res.render('index');
 });
 
 module.exports = router;
--- src/routes/purchase.js
+++ src/routes/purchase.js
@@ -3,7 +3,9 @@ const pool = require('../../database/database');
 const router = express.Router();
 const chart = require('chart.js');
 
-router.get('/purchase', async (req, res) => {
+const { isLoggedIn, isNotLoggedIn } = require('../lib/auth');
+
+router.get('/purchase', isLoggedIn, async (req, res) => {
     const purchases = await pool.query('SELECT p.id, c.name, p.date, p.totalValue FROM CLIENT c JOIN PURCHASE p ON c.id = p.client');
     const purchasesByClient = await pool.query('SELECT c.id, c.name ,SUM(p.totalValue) as Total FROM CLIENT c JOIN PURCHASE p ON c.id = p.client GROUP BY c.id');
     res.render('purchase/purchase', { purchases, purchasesByClient});
