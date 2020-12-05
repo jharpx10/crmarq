5d64ab5bd7baa3493d4b4dd01e6a570eb57239b5
Sebastian Lopez <sebastian.lopez@udea.edu.co>
2020-12-05 17:20:15 -0500

    esto se formo

--- src/routes/client.js
+++ src/routes/client.js
@@ -24,23 +24,57 @@ router.get('/validity', isLoggedIn, async (req, res) => {
     var clients = await pool.query('SELECT c.id, c.name, c.email FROM CLIENT c ');
     const fecha = new Date();
     console.log(fecha);
+
     const formato = (fecha.getDate() + "/" + (fecha.getMonth() + 1) + "/" + fecha.getFullYear());
-    await clients.forEach (async d => {
-        const purchasesClient = await pool.query('SELECT p.date FROM PURCHASE p JOIN CLIENT c on p.client = c.id AND c.id=? ORDER BY date DESC', d.id);
-        
-        if (purchasesClient[0].date < fecha) {
-            d.type = '1'
+    const fecharestada = fecha.setDate(fecha.getDate() - 15);
+    console.log("fecharestada",fecharestada);
+    for (let i = 0; i < clients.length; i++) {
+        const purchasesClient = await pool.query('SELECT p.date FROM PURCHASE p JOIN CLIENT c on p.client = c.id AND c.id=? ORDER BY date DESC', clients[i].id);
+        if (purchasesClient[0].date >= fecharestada) {
+            clients[i].type = 'bueno'
         }
         else {
-            d.type = '2'
-        }  
-        console.log(d);
+            clients[i].type = 'malo'
+        }
 
-
-    });
-    console.log("hola",clients);
+    }
+    
 
     res.json( clients );
+
+});
+router.get('/frecuency', isLoggedIn, async (req, res) => {
+    var clients = await pool.query('SELECT c.id, c.name, c.email FROM CLIENT c ');
+
+    for (let i = 0; i < clients.length; i++) {
+        const purchasesClient = await pool.query('SELECT COUNT(*) as amount FROM PURCHASE p JOIN CLIENT c on p.client = c.id AND c.id=? ORDER BY amount', clients[i].id);
+        if (purchasesClient[0].amount >=  10) {
+            clients[i].type = 'bueno'
+        }
+        else {
+            clients[i].type = 'malo'
+        }
+        
+    }
+    console.log(clients);
+    res.json(clients);
+
+});
+router.get('/volumen', isLoggedIn, async (req, res) => {
+    var clients = await pool.query('SELECT c.id, c.name, c.email FROM CLIENT c ');
+
+    for (let i = 0; i < clients.length; i++) {
+        const purchasesClient = await pool.query('SELECT SUM(p.totalValue) as value FROM PURCHASE p JOIN CLIENT c on p.client = c.id AND c.id=? ORDER BY c.id', clients[i].id);
+        if (purchasesClient[0].value >= 45000) {
+            clients[i].type = 'bueno'
+        }
+        else {
+            clients[i].type = 'malo'
+        }
+
+    }
+    console.log(clients);
+    res.json(clients);
 
 });
 
